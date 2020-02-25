3ca4bb093537156f521093fdc112fdaa2e7d454d
rajrsm <rajshakhde@gmail.com>
2020-02-25 13:52:36 +0530

    Update WebHookTwitterController.java
    cdsv

--- src/main/java/com/example/controller/WebHookTwitterController.java
+++ src/main/java/com/example/controller/WebHookTwitterController.java
@@ -23,6 +23,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 import com.google.gson.Gson;
 import com.google.gson.JsonObject;
